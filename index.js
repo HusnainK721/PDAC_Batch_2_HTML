d3b4923f47c6ba8ea87be6297de1554a996fe300
HusnainK721 <husnaink721@gmail.com>
2021-12-17 18:49:58 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,116 @@
+// //This Is My JavaScript Code....
+// console.log('Hello World');
+
+// let name = 'Zaid';
+// console.log(name);
+
+//cannot be a keyword
+//Should be meanigful
+//Cannot start with a number EX:(1name);
+
+// const firstname = "Mirza";
+//  midname="Zaid";
+//  lastname = "Baig"
+//  console.log(firstname,midname,lastname);
+
+// let interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate)
+
+
+// let name = "Mirza"; 
+// let age = 20;
+// let isApproved = true ;
+// let firstname = undefined ;
+// let lastname = null ;
+
+// let name = 'zaid';
+// let age = '20';
+// let person = {
+//     name: 'zaid',
+//     age: 20
+// };
+
+// console.log(person);
+
+// const family = {
+//  father : 'XYZ',
+//  mother : 'ABC',
+//  fmamber : '6' 
+// };
+
+// Dot Notation 
+// family.father="zzzzzz";
+
+//Bracket Notation
+// family['fmamber'] = "1520";
+// console.log(family)
+
+//.....................ARRAY.........................
+// let selectedcolor = ['red', 'blue','green', 'black','white'];
+// selectedcolor [4]= 1222;
+// console.log(selectedcolor0);
+
+//....................FUNCTION........................
+
+// function nice (name,lastname) {
+//     console.log( `hi my nam is ${name} ${lastname}`);
+// }
+// nice('zaid','baig');
+
+//.....................Ternary Operater..............
+// let peuple = 50;
+// let type = peuple > 60 ? 'Room' : 'Hall';
+//  console.log(type);
+
+//..................... Logical Operatiors .........
+// (1) && AND
+// (2) OR
+// (3) !
+
+// const age = 20;
+
+//.........&&.........
+// if (age >= 18 && age<= 40){
+//     ('This is true');
+// };
+// console.log(age);
+
+//........||..........
+// let heighIncom = false;
+// let goodCreditScore = false;
+// let eligibleForLoan = heighIncom || goodCreditScore;
+// console.log('Eligible',  eligibleForLoan);
+
+//...........Not (!).......
+// let applicationRefused = ! eligibleForLoan;
+// console.log('Application Refused', applicationRefused);
+
+//........if.....else.........
+// var age = 21;
+// var drive;
+// if(age>= 18){
+//     drive='Yes Drive';
+// }else{
+//     drive='No Drive';
+// };
+// console.log(drive);
+
+//..............ternary operater.........
+
+// var age = 10;
+// var drive= age>= 18 ?'Yes Drive' : 'No Drive';
+
+// console.log(drive);
+
+let userColor = 'undefind';
+let defaultColor = 'red';
+let currentColor = userColor || defaultColor ;
+
+console.log(currentColor);
+
+
+
+
+
+
